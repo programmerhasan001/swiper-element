862138e2185d87629a46099576aad7c4098e1a98
Hasan <programmermdhasan@gmail.com>
2023-07-23 14:43:45 +0600

    swiper thumbs

--- src/components/SliderSimple.js
+++ src/components/SliderSimple.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Thumbs } from "swiper/modules"
 
