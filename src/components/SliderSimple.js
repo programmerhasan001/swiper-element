aa7268016fac4730daa99ff7e30013f7ac176562
Hasan <programmermdhasan@gmail.com>
2023-07-23 12:39:54 +0600

    first commit

---
+++ src/components/SliderSimple.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import Swiper from 'swiper'
+
+export default function SliderSimple() {
+    useEffect(() => {
+        new Swiper('.my-swiper', {
+            loop: true,
+            slidesPerView: "auto",
+        })
+    }, [])
+    return (
+        <div className='w-2/4 mx-auto mt-28 border border-teal-700 p-20'>
+            <swiper-container
+                className="my-swiper"
+            >
+                <swiper-slide>
+                    <img src="https://cdn.pixabay.com/photo/2023/06/27/10/51/man-8091933_1280.jpg" alt="" />
+                </swiper-slide>
+                <swiper-slide>
+                    <img src="https://cdn.pixabay.com/photo/2023/04/10/06/32/tulip-7912886_1280.jpg" alt="" />
+                </swiper-slide>
+                <swiper-slide>
+                    <img src="https://cdn.pixabay.com/photo/2023/04/26/03/48/drink-7951411_1280.jpg" alt="" />
+                </swiper-slide>
+                <swiper-slide>
+                    <img src="https://cdn.pixabay.com/photo/2023/06/23/08/22/flower-8082986_1280.jpg" alt="" />
+                </swiper-slide>
+            </swiper-container>
+        </div>
+    )
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,7 @@
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
